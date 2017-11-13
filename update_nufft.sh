5ae422d2fffa2f43ae31d0c308d13faca768e71b
Gal <galsdh@gmail.com>
2017-11-13 15:26:55 +0200

    all files from original directory were removed

--- update_nufft.sh
+++
@@ -1,8 +0,0 @@
-rm nufft$1df90.pyf
-echo "done removing!"
-
-f2py nufft$1df90_f2py.f -m nufft$1df90 -h nufft$1df90.pyf
-echo "pyf file was created"
-
-f2py -c nufft$1df90.pyf nufft$1df90_f2py.f dfftpack.f next235.f
-echo "Done"
